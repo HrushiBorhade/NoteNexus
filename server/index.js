f38df27749caf229285b53b0a909c9eaca442836
Hrushikesh Borhade <hrushiborhade123@gmail.com>
2023-03-08 20:18:47 +0530

    created our server using node,express and created few api routes for getting our static data

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const notes = require("./data/notes");
+const app = express();
+dotenv.config();
+
+console.log(notes);
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.send(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((note) => note._id === req.params.id);
+  res.send(note);
+});
+
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
+});
